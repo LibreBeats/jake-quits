9a00e0c508cb2c3ec96404e8f9e13f0633c771ae
Charlie <carlos.letamendia@aitire.es>
2019-10-08 11:23:14 +0200

    Changed colours

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const data = {
   minutes: null,
   seconds: null,
   savings: null,
-  dollars: null,
+  euros: null,
   cents: null,
-  dailyCost: 6.375466667,
+  dailyCost: 5,
   time: 1569369600000 // new Date(2019, 8, 24, 19)
 };
 
@@ -28,14 +28,14 @@ function update() {
   data.minutes = seconds / 60;
   data.seconds = seconds;
   data.savings = data.days * data.dailyCost;
-  data.dollars = Math.floor(data.savings)
+  data.euros = Math.floor(data.savings)
     .toString()
     .replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   data.cents = (data.savings - Math.floor(data.savings))
     .toFixed(2)
     .split(".")[1];
   updatePies();
-  document.getElementById("dollars").innerText = data.dollars;
+  document.getElementById("euros").innerText = data.euros;
   document.getElementById("cents").innerText = data.cents;
 
   requestAnimationFrame(update);
